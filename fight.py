3a4416355a11f57638331a9bbe28f0fa1c1f3fa3
Aleksi Sillantaka <aleksi.sillantaka@gmail.com>
2022-10-22 14:41:25 +0300

    Moved Fighter creation to data.py

--- fight.py
+++ fight.py
@@ -1,26 +1,4 @@
-from dataclasses import dataclass
-
-# Class to hold fighter specs
-@dataclass
-class Fighter:
-    fighter_id: int
-    name: str
-    healthpoints: int
-    attackpoints: float
-    defencepoints: float
-    mass: int
-    attack_time: float = 0
-    alive: bool = True
-    next_attack: float = 0
-
-    # Method to calculate how frequently fighter attacks
-    def time_to_attack(self) -> None:
-        self.attack_time = self.attackpoints + self.defencepoints + self.mass
-
-    #Method to calculate when next attack is going to happen
-    def next_attack_time(self) -> None:
-        self.next_attack += self.attack_time
-        self.next_attack = round(self.next_attack,2)
+from data import Fighter
 
 class Turn:
     # Take Attackpoints and subtract defender's defencepoints to get the power of attack
@@ -36,5 +14,4 @@ class Turn:
             return f'{attack_time}s: {attacker.name} has done {attack_power} points of damage and won the battle! \n \nWINNER: {attacker.name}'
         else:
             return f'{attack_time}s: {attacker.name} has done {attack_power} points of damage! {defender.name} has {round(defender.healthpoints,2)} healthpoints left!'
-
 
